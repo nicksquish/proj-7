c3aa2ec30f5581e0ff0f115b090d49d70161430d
Nicholas Potter <pottern12@gmail.com>
2018-11-08 20:15:54 -0600

    connected foursquare api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Map from './Map'
 import './App.css'
-import axios from 'axios'
+import SquareAPI from './Foursquare'
 
 class App extends Component {
 
@@ -9,47 +9,53 @@ class App extends Component {
     super()
     this.state = {
       venues: [],
-      markers: []
+      markers: [],
+      
     }
   }
 
   componentDidMount() {
-    this.getVenues()
-    this.getMarkers()
+    SquareAPI.search({
+      near: 'Chicago, IL',
+      query: 'fun',
+      limit: 15
+    }).then(results => console.log(results))
+    // this.getVenues()
+    // this.getMarkers()
   }
 
-  getVenues = () => {
-    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
-    const parameters = {
-      client_id: 'HM4UUEGOD54Z05TFCFWL04IZURSAOJK4XE3W4O0HAWNVWJLV',
-      client_secret: '55IUQMXWYAUOVQOYTGZX50E4YAQ5VVDJK03Y3UODBFUDSMW5',
-      v: '20181019',
-      ll: '41.991431, -87.676839'
-    }
+  // getVenues = () => {
+  //   const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+  //   const parameters = {
+  //     client_id: 'HM4UUEGOD54Z05TFCFWL04IZURSAOJK4XE3W4O0HAWNVWJLV',
+  //     client_secret: '55IUQMXWYAUOVQOYTGZX50E4YAQ5VVDJK03Y3UODBFUDSMW5',
+  //     v: '20181019',
+  //     ll: '41.991431, -87.676839'
+  //   }
 
-    axios.get(endPoint + new URLSearchParams(parameters))
-      .then(response => {
-        this.setState({venues: response.data.response.groups[0].items})       
-        console.log(response)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-  getMarkers = () => {
-    const markers = this.state.venues.map(venue => {
-          return {
-            position: {lat: venue.location.lat, 
-                     lng: venue.location.lng},
-            isOpen: false,
-            isVisible: true,
-            title: venue.name,
-            id: venue.id
-          }
-        })
+  //   axios.get(endPoint + new URLSearchParams(parameters))
+  //     .then(response => {
+  //       this.setState({venues: response.data.response.groups[0].items})       
+  //       console.log(response)
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //     })
+  // }
+  // getMarkers = () => {
+  //   const markers = this.state.venues.map(venue => {
+  //         return {
+  //           position: {lat: venue.location.lat, 
+  //                    lng: venue.location.lng},
+  //           isOpen: false,
+  //           isVisible: true,
+  //           title: venue.name,
+  //           id: venue.id
+  //         }
+  //       })
 
-        this.setState({markers})
-  }
+  //       this.setState({markers})
+  // }
 
   render() {
     return (
---
+++ src/Foursquare.js
@@ -0,0 +1,52 @@
+class Helper {
+	static baseURL() {
+		return 'https://api.foursquare.com/v2'
+	}
+	static auth() {
+		const keys = {
+			client_id: 'HM4UUEGOD54Z05TFCFWL04IZURSAOJK4XE3W4O0HAWNVWJLV',
+      client_secret: '55IUQMXWYAUOVQOYTGZX50E4YAQ5VVDJK03Y3UODBFUDSMW5',
+      v: '20181019',
+      ll: '41.991431, -87.676839'
+		}
+		return Object.keys(keys)
+			.map(key => `${key}=${keys[key]}`)
+			.join('&')
+	}
+	static urlBuilder(urlPrams){
+		if(!urlPrams){
+			return ''
+		}
+		return Object.keys(urlPrams)
+			.map(key => `${key}=${urlPrams[key]}`)
+			.join('&')
+	}
+	static headers() {
+		return {
+			Accept: 'application/json'
+		}
+	}
+	static simpleFetch(endPoint, method, urlPrams){
+		let requestData = {
+			method,
+			headers: Helper.headers()
+		}
+		return fetch(
+			`${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+			urlPrams
+		)}`,
+			requestData
+		).then(res => res.json())
+	}
+}
+export default class SquareAPI {
+	static search(urlPrams){
+		return Helper.simpleFetch('/venues/search', 'GET', 'urlPrams')
+	}
+	static getVenueDetails(VENUE_ID){
+		return Helper.simpleFetch(`/venues/${VENUE_ID}`, 'GET')
+		}
+	static getVenuePhotos(VENUE_ID){
+		return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, 'GET')
+	}
+	}
